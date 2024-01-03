43535daba1d9f9cad6426a882d51ee0cd1a5dbe0
117kayalvizhi <155185624+117kayalvizhi@users.noreply.github.com>
2024-01-03 12:44:57 +0530

    Add files via upload

--- Target.java
+++ Target.java
@@ -1,5 +1,25 @@
 package Basic;
 
-public class Target {
+import java.util.Scanner;
 
-}
+public class Target {
+	static Scanner sc=new Scanner(System.in);	 
+	public static void main(String[] args) {
+		int a[]= {8,9,5,3,4,1};
+		String res="not present";
+		System.out.println("enter the key element");
+		int key=sc.nextInt();
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==key)
+			{
+				res="present";
+				break;
+				
+			}
+		}
+		System.out.println(res);
+		
+	}
+	}
+
+
